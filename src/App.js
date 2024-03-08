fa04e62463340c348278d78ba61bad5cb0ddfb49
HiHighs <degroote.jonas@hotmail.com>
2024-03-08 17:04:14 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Logo />
+      <Form />
+      <PackList />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1>✈️ Vacation Time 🌴</h1>;
+}
+
+function Form() {
+  return (
+    <div className='add-form'>
+      <h3>What to bring on the trip? 🌞</h3>
+    </div>
+  );
+}
+
+function PackList() {
+  return <div className='list'>LIST</div>;
+}
+
+function Stats() {
+  return (
+    <footer className='stats'>
+      <em>You have X items on your list, and you already packed X (X%) 🧳</em>
+    </footer>
+  );
+}
